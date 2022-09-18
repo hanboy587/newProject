48ddd65bde92f8e3f55ad7eb219bfe36b6e43496
hanboy587 <hanboy587@gmail.com>
2022-09-19 08:26:16 +0900

    lastTest

--- src/main/java/com/spring/client/memberService/MemberService.java
+++ src/main/java/com/spring/client/memberService/MemberService.java
@@ -15,7 +15,7 @@ public class MemberService {
 	private SqlSessionTemplate sqlSessionTemplate;
 
 	private String pushTest2;
-	private String pushTestOneMore3;
+	private String pushTestOneMore4;
 	
 	public List<String> memberList(MemberVO memberVO){
 		return sqlSessionTemplate.selectList("memberList");
