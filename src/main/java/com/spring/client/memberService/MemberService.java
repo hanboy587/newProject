eac306b644156b0f9668321da50f6fa10cc5c9ac
hanboy587 <hanboy587@gmail.com>
2022-09-19 15:10:49 +0900

    lastTest

--- src/main/java/com/spring/client/memberService/MemberService.java
+++ src/main/java/com/spring/client/memberService/MemberService.java
@@ -15,7 +15,7 @@ public class MemberService {
 	private SqlSessionTemplate sqlSessionTemplate;
 
 	private String pushTest2;
-	private String pushTestOneMore16;
+	private String pushTestOneMore17;
 	
 	public List<String> memberList(MemberVO memberVO){
 		return sqlSessionTemplate.selectList("memberList");
